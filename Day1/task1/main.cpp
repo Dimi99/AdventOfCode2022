3865d772273c34fc6b2961cfc12e57979fedd243
Dimi <dimitrios_K@gmx.de>
2022-12-01 14:15:12 +0100

    task2

--- Day1/task1/main.cpp
+++ Day1/task1/main.cpp
@@ -9,42 +9,49 @@
 #include <numeric>
 #include <cstring>
 #include <algorithm>
+#include <queue>
 
 class ElveCommunity{
 public:
     ElveCommunity(std::ifstream &in){
         char bufX[10];
-        individuals.emplace_back();
+        std::vector<int> food_list;
         while(in) {
             in.getline(bufX, sizeof(std::string), '\n');
             int calorie = atoi(bufX);
-            if (calorie)
-                individuals.back().add_calorie(calorie);
-            else
-                individuals.emplace_back();
+            if (calorie) {
+                food_list.push_back(calorie);
+                continue;
+            }
+
+            individuals.emplace(ElveCommunity::Elve(food_list));
+            food_list.clear();
 
         }
-        std::for_each(individuals.begin(), individuals.end(), [&](Elve &elve) {
-           elve.calc_calories();
-        });
-    }
 
-    int max_element(){return std::max_element(individuals.begin(),individuals.end(), std::greater<>())->total_calories();}
+    }
 
     struct Elve {
 public:
+        explicit Elve(const std::vector<int> &food_list):
+        foods_(food_list)
+        {
+            total_calories_ = std::accumulate(foods_.begin(), foods_.end(), 0);
+        }
         void add_calorie(int food){foods_.push_back(food);}
         void calc_calories(){total_calories_ = std::accumulate(foods_.begin(), foods_.end(), 0);}
         int total_calories () const {return total_calories_;}
-        bool operator>(const Elve& elve) const
+        bool operator<(const Elve& elve) const
         {return this->total_calories() < elve.total_calories();}
 private:
         int total_calories_;
         std::vector<int> foods_;
     };
+Elve max_elve(){return individuals.top();}
+void pop(){individuals.pop();}
 
 private:
-    std::vector<Elve> individuals;
+    std::priority_queue<Elve> individuals;
 
 };
 
@@ -53,7 +60,13 @@ int main() {
 
     std::ifstream input(FILEPATH);
     ElveCommunity elves(input);
-    // max amound of calories
-    std::cout << elves.max_element();
+    // Top 3 Elves
+    auto result = 0;
+    for(int i = 0; i <=2; ++i) {
+         result += elves.max_elve().total_calories();
+        elves.pop();
+    }
+    std::cout << "Result: " << result << std::endl;
+
     return 0;
 }
