66326033865bfd475981047326924214186540fe
Anita <AnitaGoo@mail.ru>
2022-12-03 10:39:39 +0100

    day3 py

--- Day3/Anita_py/day3.py
+++ Day3/Anita_py/day3.py
@@ -2,6 +2,7 @@ with open('input.txt') as f:
     chars = [char for line in f for char in set(line[:len(line)//2]).intersection(set(line[len(line)//2:]))]
     print(sum([ord(char)-96 if char.islower() else ord(char)-38 for char in chars]))
 
+# part 2
 with open('input.txt') as f:
     lines = [line.strip() for line in f]
     chars = [char for i in range(0, len(lines), 3)
