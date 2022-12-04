f86b20aedbcc7af74ea5a8146bbbf2b26b542d94
Anita <AnitaGoo@mail.ru>
2022-12-04 14:59:44 +0100

    whadafck

---
+++ Day4/Anita_py/day4.py
@@ -0,0 +1,10 @@
+import re
+
+with open('input.txt') as f:
+    n_contains = 0
+    for line in f:
+        n1, n2, n3, n4 = map(int, re.split(r'[-,]', line.strip()))
+        ins = {n1: 0, n2: 0, n3: 1, n4: 1}
+        if n1 == n3 or n2 == n4 or ins[max(n1, n3)] != ins[max(n2, n4)]:
+            n_contains += 1
+    print(n_contains)
